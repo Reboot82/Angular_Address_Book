ae76e97eeb0a9deffde62772e07817faaa778206
Brandt Campbell <brandtcampbell@Brandts-MacBook-Air.local>
2021-09-11 00:28:55 -0400

    working prototype completed. 30% done

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,15 +1,52 @@
+import { stripSummaryForJitNameSuffix } from '@angular/compiler/src/aot/util';
 import { Component, OnInit } from '@angular/core';
+import { Contact } from './../../models/Contact';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
 })
 export class ContactsComponent implements OnInit {
+  contacts: Contact[] | undefined;
 
-  constructor() { }
+  inputContact: string = '';
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.contacts = [
+      {
+        content: 'First Contact',
+        favorite: false,
+      },
+      {
+        content: 'Brian Starnes',
+        favorite: true,
+      },
+    ];
   }
 
+  toggleFavorite(id: number): void {
+    this.contacts?.map((v, i) => {
+      if (i == id) v.favorite = !v.favorite;
+
+      return v;
+    });
+  }
+
+  deleteContact(id: number) {
+    this.contacts = this.contacts?.filter((v, i) => i !== id);
+  }
+
+  addContact() {
+    if (this.inputContact != '') {
+      this.contacts?.push({
+        content: this.inputContact,
+        favorite: false,
+      });
+    }
+
+    this.inputContact = '';
+  }
 }
---
+++ src/app/models/Contact.ts
@@ -0,0 +1,4 @@
+export class Contact {
+    content: string = '';
+    favorite: boolean = false;
+}
