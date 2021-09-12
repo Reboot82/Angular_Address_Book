b70f5843b822ffcc0d11bee8bb7831a46c9939f7
Brandt Campbell <brandtcampbell@Brandts-MacBook-Air.local>
2021-09-11 21:12:14 -0400

    save point 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContactsComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, ContactsComponent, DashboardComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -27,34 +27,7 @@ export class ContactsComponent implements OnInit {
 
   ngOnInit(): void {
     this.contacts = [
-      {
-        customerId: 'ABCDEF',
-        companyName: 'CompanyName, LLC',
-        contactName: 'John Businessman',
-        contactTitle: 'Bossdude',
-        address: '123 Madeup Rd.',
-        city: 'Blam',
-        email: 'fiuvnwevl@gmail.com',
-        postalCode: '12345',
-        favorite: false,
-        country: 'N/A',
-        phone: '234-456-2345',
-        fax: '352252354',
-      },
-      {
-        customerId: 'ABCDEFG',
-        companyName: 'CompanyName, Inc.',
-        contactName: 'John Q. Businessman',
-        contactTitle: 'Upper Management',
-        address: '1234 Madeup Rd.',
-        city: 'Wham',
-        email: 'wericubqer@gmail.com',
-        postalCode: '22345',
-        favorite: true,
-        country: 'America',
-        phone: '234-876-2945',
-        fax: '234-567-4567',
-      },
+      
     ];
   }
 
